a865f2aece61c00e9bf08c644c787fdac78a34a8
vincentm1214 <144461068+vincentm1214@users.noreply.github.com>
2023-12-12 10:11:41 -0500

    9

--- api-project/public/js/main.js
+++ api-project/public/js/main.js
@@ -1,36 +1,4 @@
-// //get a promise
-// function greet(name){
-//     const greetPromise=new Promise(function(resolve,reject){
-//         resolve(`Hello ${name}`);
-//     });
-//         return greetPromise;
-//     }
-
-// const mandy=greet("Mandy");
-// //handle the promise
-// mandy.then((result)=>{
-//     //putting data into html
-//     console.log(result);
-// });
-// const URL=`https://api.quotable.io/random`;
-// // const URLP=`pokemom/${search}`;
-// async function getData(URL){
-//     try {
-//         //requesting a response REST API's
-//         const response=await fetch(URL);
-//         if(response.status !=200){
-//             throw new Error(response.statusText)
-//         }
-//         //convert response to JSON
-//         const data=await response.json();
-//         document.querySelector("h1").textContent=data.content;
-//     } catch (error) {
-
-
-//     }
-// }
-// getData(URL);
-
+import '../style/style.css'
 const URL="https://corsproxy.io/?https://opentdb.com/api.php?amount=10";
 
 async function question(URL) {

942ccc0e37a1e4f1fdd0b96c83ca127af7b59c57
vincentm1214 <144461068+vincentm1214@users.noreply.github.com>
2024-01-02 10:15:21 -0500

    16

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,38 +1,16 @@
-import { DOMSelectors } from "./dom";
-
+const DOMSelectors={
+    body:document.querySelector(".body"),
+    card:document.querySelector('#card'),
+    app:document.querySelector('#container'),
+    org:document.querySelector('#question'),
+    ger:document.querySelector('#answer'),
+    lop:document.querySelector('#wrong'),
+    cof:document.querySelector('#difficulty'),
+    abf:document.querySelector('#category'),
+}
 //   import '../style/style.css'
 const URL="https://corsproxy.io/?https://opentdb.com/api.php?amount=10";
-
-//  function insertCards(quiz){
-//      quiz.forEach(x) => {
-//     DOMSelectors.container.insertAdjacentHTML(
-//      "beforeend",
-//      `<div class="card">
-//          <h3 class = "name">${x.name}</h3>
-//          <img src="${x.image}" class="cardimg">
-//          <h4 class = ${x.answer}></h4>
-//      </div> `)}}
-
-function makecard(){
-    let org= DOMSelectors.question.value;
-    let ger=DOMSelectors.answer.value;
-    let lop=DOMSelectors.wrong.value;
-    let cof=DOMSelectors.hardness.value;
-    let abf=DOMSelectors.choice.value;
-    DOMSelectors.makecard.insertAdjacentHTML("beforeend",
-`<div class="food">
-<h3> question: ${org}</h3>
-<h3> answer: ${ger}</h3>
-<h3> choices: ${abf}</h3>
-<h3> wrong: ${lop}</h3>
-<h3> hardness: ${cof}</h3>
-</div>`);
-}
-
-
 async function question(URL) {
-    //const image = await fetch(URL);
-    //console.log(image);
     try {
         //         //requesting a response REST API's
         const response=await fetch(URL);
@@ -50,5 +28,32 @@ async function question(URL) {
 }
 
 question(URL);
+
+//  function insertCards(quiz){
+//      quiz.forEach(x) => {
+//     DOMSelectors.container.insertAdjacentHTML(
+//      "beforeend",
+//      `<div class="card">
+//          <h3 class = "name">${x.name}</h3>
+//          <img src="${x.image}" class="cardimg">
+//          <h4 class = ${x.answer}></h4>
+//      </div> `)}}
+
+function makecard(e){
+    let org= DOMSelectors.question.value;
+    let ger=DOMSelectors.answer.value;
+    let lop=DOMSelectors.wrong.value;
+    let cof=DOMSelectors.hardness.value;
+    let abf=DOMSelectors.choice.value;
+    DOMSelectors.card.insertAdjacentHTML("beforeend",
+`<div class="laquestion">
+<h3> question: ${org}</h3>
+<h3> answer: ${ger}</h3>
+<h3> choices: ${abf}</h3>
+<h3> wrong: ${lop}</h3>
+<h3> hardness: ${cof}</h3>
+</div>`); 
+}
+
 
 

dac477828fdde27a50b582cc32df7e59330c0dd2
vincentm1214 <144461068+vincentm1214@users.noreply.github.com>
2024-01-08 09:36:06 -0500

    :(

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,33 +1,47 @@
-import { DOMSelectors } from "./dommy";
-   import '../style/style.css'
-
-
-// let url = new URL('https://corsproxy.io/?https://opentdb.com/api.php?amount=10');
- const link="https://corsproxy.io/?https://opentdb.com/api.php?amount=10";
-async function question(link) {
+const DOMSelectors={
+  body:document.querySelector(".body"),
+  card:document.querySelector('#card'),
+  app:document.querySelector('#container'),
+  org:document.querySelector('#question'),
+  ger:document.querySelector('#answer'),
+ 
+ 
+ }
+ //   import '../style/style.css'
+ const URL="https://corsproxy.io/?https://opentdb.com/api.php?amount=10";
+ async function question(URL) {
   try {
       //         //requesting a response REST API's
-      const response=await fetch(link);
+      const response=await fetch(URL);
              if(response.status !=200){
                  throw new Error("u suck"+response.status)
              }
               //convert response to JSON
              const data=await response.json();
-             data.forEach(obj=>{
-              DOMSelectors.app.insertAdjacentHTML(
-                  ("beforeend",
-                  `<div class="laquestion">
-                  <h2> question: ${obj.org}</h2>
-                  <h3> answer: ${obj.ger}</h3>
-                  </div>`))
-             })
           //    document.getElementById("api-response").textContent=data.content;
-             console.log(data)
-             data.results.forEach(question => console.log(question));
+             console.log(data.results);
+             makecard(data.results);
+ //             data.results.forEach(question =>[
+              console.log(question.question),
+              console.log(question.answer)
+             ;
               document.querySelector('.apple').innerHTML = `<p>${data.results}</p>`;
          } catch (error) {
-
          }
-}
-
-question(link);
+ }
+ 
+ 
+ console.log(data.results)
+ question(URL);
+ 
+ 
+ function makecard(questions) {
+ questions.forEach(question => {
+  DOMSelectors.card.insertAdjacentHTML(
+    "beforeend",
+ `<div class="laquestion">
+      <h2 class="question">${question.question}</h2>
+      <h3 class="answer">${question.correct_answer}</h3>
+ </div>`);
+ })};
+ 
